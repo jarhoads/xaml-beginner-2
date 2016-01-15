1b1729ee65d6f3af8b775d629d016523e3384708
Josh <jarhoads@gmail.com>
2016-01-15 09:54:53 -0500

    Adding INotifyPropertyChanged interface
    Implemented INotifyPropertyChanged interface to update the UI when data
    is loaded

--- RestaurantManager.Models/OrderDataManager.cs
+++ RestaurantManager.Models/OrderDataManager.cs
@@ -6,13 +6,18 @@ namespace RestaurantManager.Models
     {       
         protected override void OnDataLoaded()
         {
+            
             this.MenuItems = base.Repository.StandardMenuItems;
+            // adding OnPropertyChanged for MenuItems to update the UI
+            this.OnPropertyChanged("MenuItems");
 
             this.CurrentlySelectedMenuItems = new List<MenuItem>
             {
                 this.MenuItems[3],
                 this.MenuItems[5]
             };
+            // adding OnPropertyChanged for CurrentlySelectedMenuItems to update the UI
+            this.OnPropertyChanged("CurrentlySelectedMenuItems");
         }
 
         public List<MenuItem> MenuItems { get; set; }
